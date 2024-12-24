9b1e68060db2f4aab7497a62527509a2f9003c7d
maksabujbd <mak.sabujbd@gmail.com>
2024-12-24 18:39:22 +0600

    update health status new

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok! Hurry! It's working");
+  res.status('200').send("Status: ok! Hurry! Alhamdulillah");
 });
 
 // retrieve all musicians from data store
